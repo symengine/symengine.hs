887f050ee99133ad01c749e2e8c156d0d4745377
bollu <siddu.druid@gmail.com>
2016-03-04 12:28:09 +0530

    added better comments to the library

--- src/Lib.hs
+++ src/Lib.hs
@@ -7,7 +7,11 @@ import Foreign.C.Types
 import Foreign.Ptr
 import Foreign.C.String
 
+
 -- exported functions
+
+-- |The `ascii_art_str` function prints SymEngine in ASCII art.
+-- this is useful as a sanity check
 ascii_art_str :: IO String
 ascii_art_str = ascii_art_str_raw >>= peekCString
 
