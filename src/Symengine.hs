717fd18bc5ea556b559c6fa8609f32b81e0c0598
bollu <siddu.druid@gmail.com>
2016-05-09 02:58:21 +0530

    add some basic type constructors and test them as well

--- src/Symengine.hs
+++ src/Symengine.hs
@@ -5,6 +5,8 @@ module Symengine
      ascii_art_str,
      basic_str,
      basic_const_zero,
+     basic_const_one,
+     basic_const_minus_one,
      BasicPtr,
     ) where
 
@@ -35,34 +37,52 @@ instance Show BasicPtr where
 
 
 basic_const_zero :: IO BasicPtr
-basic_const_zero = do
+basic_const_zero = basic_obj_constructor basic_const_zero_ffi
+
+
+basic_const_one :: IO BasicPtr
+basic_const_one = basic_obj_constructor basic_const_one_ffi
+
+basic_const_minus_one :: IO BasicPtr
+basic_const_minus_one = basic_obj_constructor basic_const_minus_one_ffi
+
+basic_const_I :: IO BasicPtr
+basic_const_I = basic_obj_constructor basic_const_I_ffi
+
+
+basic_obj_constructor :: (Ptr BasicStruct -> IO ()) -> IO BasicPtr
+basic_obj_constructor init_fn = do
     basic_ptr <- create_basic_ptr
-    withForeignPtr (fptr basic_ptr) basic_const_zero_raw
+    withForeignPtr (fptr basic_ptr) init_fn
     return $ basic_ptr
 
 basic_str :: BasicPtr -> String
-basic_str basic_ptr = unsafePerformIO $ withForeignPtr (fptr basic_ptr) (\p -> basic_str_raw p >>= peekCString)
+basic_str basic_ptr = unsafePerformIO $ withForeignPtr (fptr basic_ptr) (\p -> basic_str_ffi p >>= peekCString)
 
 -- |The `ascii_art_str` function prints SymEngine in ASCII art.
 -- this is useful as a sanity check
 ascii_art_str :: IO String
-ascii_art_str = ascii_art_str_raw >>= peekCString
+ascii_art_str = ascii_art_str_ffi >>= peekCString
 
--- Unexported raw functions------------------------
+-- Unexported ffi functions------------------------
 
 -- |Create a basic object that represents all other objects through
 -- the FFI
 create_basic_ptr :: IO BasicPtr
 create_basic_ptr = do
     basic_ptr <- newArray [BasicStruct { data_ptr = nullPtr }]
-    basic_new_heap_raw basic_ptr
-    finalized_ptr <- newForeignPtr ptr_basic_free_heap_raw basic_ptr
+    basic_new_heap_ffi basic_ptr
+    finalized_ptr <- newForeignPtr ptr_basic_free_heap_ffi basic_ptr
     return $ BasicPtr { fptr = finalized_ptr }
 
 
 
-foreign import ccall "symengine/cwrapper.h ascii_art_str" ascii_art_str_raw :: IO CString
-foreign import ccall "symengine/cwrapper.h basic_new_heap" basic_new_heap_raw :: Ptr BasicStruct -> IO ()
-foreign import ccall "symengine/cwrapper.h &basic_free_heap" ptr_basic_free_heap_raw :: FunPtr(Ptr BasicStruct -> IO ())
-foreign import ccall "symengine/cwrapper.h basic_const_zero" basic_const_zero_raw :: Ptr BasicStruct -> IO ()
-foreign import ccall "symengine/cwrapper.h basic_str" basic_str_raw :: Ptr BasicStruct -> IO CString
+foreign import ccall "symengine/cwrapper.h ascii_art_str" ascii_art_str_ffi :: IO CString
+foreign import ccall "symengine/cwrapper.h basic_new_heap" basic_new_heap_ffi :: Ptr BasicStruct -> IO ()
+foreign import ccall "symengine/cwrapper.h &basic_free_heap" ptr_basic_free_heap_ffi :: FunPtr(Ptr BasicStruct -> IO ())
+foreign import ccall "symengine/cwrapper.h basic_const_zero" basic_const_zero_ffi :: Ptr BasicStruct -> IO ()
+foreign import ccall "symengine/cwrapper.h basic_const_one" basic_const_one_ffi :: Ptr BasicStruct -> IO ()
+foreign import ccall "symengine/cwrapper.h basic_const_minus_one" basic_const_minus_one_ffi :: Ptr BasicStruct -> IO ()
+foreign import ccall "symengine/cwrapper.h basic_const_I" basic_const_I_ffi :: Ptr BasicStruct -> IO ()
+
+foreign import ccall "symengine/cwrapper.h basic_str" basic_str_ffi :: Ptr BasicStruct -> IO CString
--- test/Spec.hs
+++ test/Spec.hs
@@ -4,6 +4,7 @@ import Test.Tasty.HUnit as HU
 
 import Data.List
 import Data.Ord
+import Data.Monoid
 
 import Symengine as Sym
 
@@ -19,11 +20,19 @@ tests = testGroup "Tests" [unitTests]
 -- properties :: TestTree
 -- properties = testGroup "Properties" [qcProps]
 
-unitTests = testGroup "Unit tests"
-  [ HU.testCase "FFI Sanity Check - ASCII Art should be non-empty" $ 
+unitTests = testGroup "FFI Sanity Checks"
+  [ HU.testCase "ASCII Art should be non-empty" $ 
     do
       ascii_art <- Sym.ascii_art_str
-      zero <- basic_const_zero
-      print $ show zero
       HU.assertBool "ASCII art from ascii_art_str is empty" (not . null $ ascii_art)
+
+
+    , HU.testCase "Basic Constructors" $
+    do
+      zero <- basic_const_zero
+      one <- basic_const_one
+      minus_one <- basic_const_minus_one
+      "0" @?= (show 0)     
+      "1" @?= (show one)     
+      "-1" @?= (show minus_one)     
   ]
