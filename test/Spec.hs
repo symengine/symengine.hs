0d762937f0ec7d4e6047c3d9ec3236e6e3b84fb7
Tom Westerhout <14264576+twesterhout@users.noreply.github.com>
2023-01-06 01:27:36 +0100

    Remove all tests except ascii art

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,12 +1,10 @@
-import Test.Tasty
-import Test.Tasty.QuickCheck as QC
-import Test.Tasty.HUnit as HU
-
 import Data.List
-import Data.Ord
 import Data.Monoid
-
+import Data.Ord
 import Symengine as Sym
+import Test.Tasty
+import Test.Tasty.HUnit as HU
+import Test.Tasty.QuickCheck as QC
 import Prelude hiding (pi)
 
 main = defaultMain tests
@@ -14,40 +12,39 @@ main = defaultMain tests
 tests :: TestTree
 tests = testGroup "Tests" [unitTests]
 
-
 -- These are used to check invariants that can be tested by creating
 -- random members of the type and then checking invariants on them
 
 -- properties :: TestTree
 -- properties = testGroup "Properties" [qcProps]
 
-unitTests = testGroup "Unit tests"
-  [ HU.testCase "FFI Sanity Check - ASCII Art should be non-empty" $ 
-    do
-      ascii_art <- Sym.ascii_art_str
-      HU.assertBool "ASCII art from ascii_art_str is empty" (not . null $ ascii_art)
+unitTests =
+  testGroup
+    "Unit tests"
+    [ HU.testCase "FFI Sanity Check - ASCII Art should be non-empty" $
+        do
+          ascii_art <- Sym.ascii_art_str
+          HU.assertBool "ASCII art from ascii_art_str is empty" (not . null $ ascii_art)
 
+          -- , HU.testCase "Basic Constructors" $
+          -- do
+          --   "0" @?= (show zero)
+          --   "1" @?= (show one)
+          --   "-1" @?= (show minus_one)
+          -- , HU.testCase "Basic Trignometric Functions" $
+          -- do
+          --   let pi_over_3 = pi / 3 :: BasicSym
+          --   let pi_over_2 = pi / 2 :: BasicSym
 
-    , HU.testCase "Basic Constructors" $
-    do
-      "0" @?= (show zero)     
-      "1" @?= (show one)     
-      "-1" @?= (show minus_one)
-    , HU.testCase "Basic Trignometric Functions" $
-    do
-      let pi_over_3 = pi / 3 :: BasicSym
-      let pi_over_2 = pi / 2 :: BasicSym
+          --   sin zero @?= zero
+          --   cos zero @?= one
+          --
+          --   sin (pi / 6) @?= 1 / 2
+          --   sin (pi / 3) @?= (3 ** (1/2)) / 2
 
-      sin zero @?= zero
-      cos zero @?= one
-      
-      sin (pi / 6) @?= 1 / 2
-      sin (pi / 3) @?= (3 ** (1/2)) / 2
+          --   cos (pi / 6) @?= (3 ** (1/2)) / 2
+          --   cos (pi / 3) @?= 1 / 2
 
-      cos (pi / 6) @?= (3 ** (1/2)) / 2
-      cos (pi / 3) @?= 1 / 2 
-
-      sin pi_over_2 @?= one
-      cos pi_over_2 @?= zero
-
-  ]
+          --   sin pi_over_2 @?= one
+          --   cos pi_over_2 @?= zero
+    ]
