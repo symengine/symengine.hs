42e6d713bb23f68a7a778db8f426dbecbd82af70
twesterhout <14264576+twesterhout@users.noreply.github.com>
2023-06-19 12:12:50 +0200

    Update the flake to use packageOverrides to support multiple GHC versions

--- flake.nix
+++ flake.nix
@@ -18,7 +18,17 @@
   outputs = inputs: inputs.flake-utils.lib.eachDefaultSystem (system:
     with builtins;
     let
-      inherit (inputs.nixpkgs) lib;
+      src = inputs.nix-filter.lib {
+        root = ./.;
+        include = [
+          "src"
+          "test"
+          "symengine.cabal"
+          "README.md"
+          "LICENSE"
+        ];
+      };
+
       pkgs = import inputs.nixpkgs {
         inherit system;
         overlays = [
@@ -36,83 +46,54 @@
                 "-DBUILD_SHARED_LIBS=ON"
               ];
             });
+
+            haskell = super.haskell // {
+              packageOverrides = hself: hsuper: {
+                symengine = (hself.callCabal2nix "symengine" src {
+                  inherit (self) symengine;
+                  mpc = self.libmpc;
+                });
+              };
+            };
           })
         ];
+
+        config.allowBroken = true;
       };
 
-      src = inputs.nix-filter.lib {
-        root = ./.;
-        include = [
-          "src"
-          "test"
-          "symengine.cabal"
-          "README.md"
-          "LICENSE"
-        ];
-      };
 
-      # This allows us to build a Haskell package with any given GHC version.
-      # It will also affects all dependent libraries.
-      # overrides allows us to patch existing Haskell packages, or introduce new ones
-      # see here for specifics: https://nixos.wiki/wiki/Overlays
-      haskellPackagesOverride = ps: args:
-        ps.override
-          {
-            overrides = self: super: {
-              symengine = (self.callCabal2nix "symengine" src {
-                inherit (pkgs) symengine;
-                mpc = pkgs.libmpc;
-              });
-            };
-          };
-
-      outputsFor =
-        { haskellPackages
-        , name
-        , package ? ""
-        , ...
-        }:
-        let
-          ps = haskellPackagesOverride haskellPackages { };
-        in
-        {
-          packages.${name} = ps.${package} or ps;
-          devShells.${name} = ps.shellFor {
-            packages = ps: with ps; [
-              symengine
-            ];
-            withHoogle = true;
-            nativeBuildInputs = with pkgs; with ps; [
-              # Building and testing
-              cabal-install
-              # Language servers
-              haskell-language-server
-              nil
-              # Formatters
-              fourmolu
-              cabal-fmt
-              nixpkgs-fmt
-              # Previewing markdown files
-              python3Packages.grip
-            ];
-            shellHook = ''
-              LD_LIBRARY_PATH=${pkgs.symengine}/lib:${pkgs.flint}/lib:${pkgs.libmpc}/lib:${pkgs.mpfr}/lib:$LD_LIBRARY_PATH
-              SYMENGINE_PATH=${pkgs.symengine}
-            '';
-          };
-          # The formatter to use for .nix files (but not .hs files)
-          # Allows us to run `nix fmt` to reformat nix files.
-          formatter = pkgs.nixpkgs-fmt;
-        };
     in
-    foldl' (acc: conf: lib.recursiveUpdate acc (outputsFor conf)) { }
-      (lib.mapAttrsToList (name: haskellPackages: { inherit name haskellPackages; })
-        (lib.filterAttrs (_: ps: ps ? ghc) pkgs.haskell.packages) ++ [
-        {
-          haskellPackages = pkgs.haskellPackages;
-          name = "default";
-          package = "symengine";
-        }
-      ])
+    {
+      packages = {
+        default = pkgs.haskellPackages.symengine;
+        symengine = pkgs.haskellPackages.symengine;
+        haskell = pkgs.haskell.packages;
+      };
+
+      devShells.default = haskellPackages.shellFor {
+        packages = ps: with ps; [ symengine ];
+        withHoogle = true;
+        nativeBuildInputs = with pkgs; with ps; [
+          # Building and testing
+          cabal-install
+          # Language servers
+          haskell-language-server
+          nil
+          # Formatters
+          fourmolu
+          cabal-fmt
+          nixpkgs-fmt
+          # Previewing markdown files
+          python3Packages.grip
+        ];
+        shellHook = ''
+          LD_LIBRARY_PATH=${pkgs.symengine}/lib:${pkgs.flint}/lib:${pkgs.libmpc}/lib:${pkgs.mpfr}/lib:$LD_LIBRARY_PATH
+          SYMENGINE_PATH=${pkgs.symengine}
+        '';
+      };
+      # The formatter to use for .nix files (but not .hs files)
+      # Allows us to run `nix fmt` to reformat nix files.
+      formatter = pkgs.nixpkgs-fmt;
+    }
   );
 }
